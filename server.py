be9a69d6c2018d440148cd085ce4645df97caad9
Humair Khan <humair88@hotmail.com>
2019-09-25 08:15:47 -0400

    added a cleaner 'hack' to import remaining static files

--- server.py
+++ server.py
@@ -9,32 +9,49 @@ def main():
 
 @app.route("/mock/orderService")
 def order():
-    data = {"table": [
-        {
-            "id": "1",
-            "pname": "bananas",
-            "pprice": 5,
-            "ptype": "fruit",
-            "image": "bananas.jpg",
-            "pquant": 1
-        },
-        {
-            "id": "2",
-            "image": "onions.jpg",
-            "pname": "onions",
-            "pprice": 3,
-            "ptype": "vegetables",
-            "pquant": 1
-        },
-        {
-            "id": "3",
-            "image": "milk.jpg",
-            "pname": "milk",
-            "pprice": 4,
-            "ptype": "dairy",
-            "pquant": 1
-        }
-    ]
+    data = {
+        "products": [
+            {
+                "id": "79161a98-30e0-11e7-b4e8-9801a798fc8f",
+                "pname": "bananas",
+                "pprice": 5.00,
+                "ptype": "fruit",
+                "image": "bananas.jpg",
+                "pquant": 1
+            },
+            {
+                "id": "7915f0cc-30e0-11e7-91c7-9801a798fc8f",
+                "image": "onions.jpg",
+                "pname": "onions",
+                "pprice": 3.00,
+                "ptype": "vegetables",
+                "pquant": 1
+            },
+            {
+                "id": "79169ffe-30e0-11e7-bf3b-9801a798fc8f",
+                "image": "milk.jpg",
+                "pname": "milk",
+                "pprice": 4.00,
+                "ptype": "dairy",
+                "pquant": 1
+            },
+            {
+                "id": "7916d9ba-30e0-11e7-b66f-9801a798fc8f",
+                "image": "cheese.jpg",
+                "pname": "cheese",
+                "pprice": 3.00,
+                "ptype": "dairy",
+                "pquant": 3
+            },
+            {
+                "id": "79165436-30e0-11e7-b79a-9801a798fc8f",
+                "image": "almonds.jpg",
+                "pname": "almonds",
+                "pprice": 10.00,
+                "ptype": "nuts",
+                "pquant": 1
+            }
+        ]
     }
     response = app.response_class(
         response=json.dumps(data),
@@ -47,27 +64,23 @@ def order():
 # TODO: A proper fix for css/ico structuring to automatically serve them from satic_folder
 # TODO: Directly importing css into the jsx code will eliminate this issue
 @app.route('/favicon.ico')
-def sendIco():
+def send_ico():
     return send_from_directory(app.root_path + '/build/', 'favicon.ico', conditional=True)
 
+
 @app.route('/logo.svg')
-def sendLogo():
+def send_logo():
     return send_from_directory(app.root_path + '/build/', 'logo.svg', conditional=True)
 
 
-@app.route("/css/patternfly-additions.css")
-def sendPF():
-    return send_from_directory(app.root_path + '/build/', "css/patternfly-additions.css", conditional=True)
+@app.route("/css/<filename>")
+def send_css(filename):
+    return send_from_directory(app.root_path + '/build/', "css/" + filename, conditional=True)
 
 
-@app.route("/css/react-bootstrap-table.css")
-def sendBS():
-    return send_from_directory(app.root_path + '/build/', "css/patternfly-additions.css", conditional=True)
-
-
-@app.route("/css/main.css")
-def sendMainCSS():
-    return send_from_directory(app.root_path + '/build/', "css/main.css", conditional=True)
+@app.route("/images/<filename>")
+def send_images(filename):
+    return send_from_directory(app.root_path + '/build/', "images/" + filename, conditional=True)
 
 
 app.debug=True
