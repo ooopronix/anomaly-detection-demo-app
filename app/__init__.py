21ff6ab24d2053a2c6ea3a248e6b6cc057df9ff0
Anand Sanmukhani <anand@localhost.localdomain>
2019-09-26 19:48:54 +0000

    Add promethus_flask_exporter to export flask metrics

--- app/__init__.py
+++ app/__init__.py
@@ -3,16 +3,21 @@ import os
 from flask import Flask, render_template, send_from_directory, json
 from .views.mock import mock
 from .views.index import index_blueprint
-from .views.metrics import metrics
+from prometheus_flask_exporter import PrometheusMetrics
+
 
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, static_folder="build/static", template_folder="build")
+
     app.config.from_object('config')
 
     app.register_blueprint(mock)
     app.register_blueprint(index_blueprint)
-    app.register_blueprint(metrics)
 
+    # set up prometheus metrics exporting
+    metrics = PrometheusMetrics(app)
+    # static information as metric
+    metrics.info('AnomalyDetectorDemo', 'Demo application for PAD/LAD', version='0.1')
     return app
