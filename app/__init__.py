ac763cb80be368d308be95822d3bb31da8ec7f78
Humair Khan <humair88@hotmail.com>
2019-09-27 00:46:12 -0400

    added logs, ref #8

--- app/__init__.py
+++ app/__init__.py
@@ -4,12 +4,29 @@ from flask import Flask, render_template, send_from_directory, json
 from .views.mock import mock
 from .views.index import index_blueprint
 from prometheus_flask_exporter import PrometheusMetrics
-
+from flask.logging import default_handler
+from logging.handlers import RotatingFileHandler
+import logging
 
 
 def create_app(test_config=None):
+
     # create and configure the app
     app = Flask(__name__, static_folder="build/static", template_folder="build")
+
+    dir_name = 'logs'
+    try:
+        # Create log Directory
+        os.mkdir(dir_name)
+        print("Directory ", dir_name, " Created ")
+    except FileExistsError:
+        print("Directory ", dir_name, " already exists")
+
+    # app.logger.removeHandler(default_handler)
+    handler = RotatingFileHandler('logs/orders.log', maxBytes=10000, backupCount=1)
+    handler.setLevel(logging.DEBUG)
+    app.logger.addHandler(handler)
+    app.logger.setLevel(logging.DEBUG)
 
     app.config.from_object('config')
 
--- app/views/mock.py
+++ app/views/mock.py
@@ -1,11 +1,12 @@
 # facebook/views/profile.py
 
-from flask import Blueprint, json, current_app as app
+from flask import Blueprint, json, current_app as app, request
+import re
 
 mock = Blueprint('mock', __name__, url_prefix='/mock')
 
 
-@mock.route('/orderService')
+@mock.route('/orderService', methods=['GET'])
 def order():
     data = {
         "products": [
@@ -53,6 +54,35 @@ def order():
     }
     response = app.response_class(
         response=json.dumps(data),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+
+@mock.route('/orderService', methods=['POST'])
+def log_order():
+    res = request.form.to_dict(flat=False)
+
+    # Todo: Figure out why data is unstructured
+    # Rebuild data
+    i = 1
+    products = []
+    product = {}
+    for key in request.form:
+        m = re.search(r"\[([A-Za-z0-9_]+)\]", key)
+        # Get a new item every 7 keys
+        if i % 7 == 0:
+            products.append(product)
+            product = {}
+        product[m.group(1)] = request.form[key]
+        i += 1
+
+    for prod in products:
+        app.logger.info("Order placed for {} items of {} which costs {}".format(prod['pquant'], prod['pname'], prod['pprice']))
+
+    response = app.response_class(
+        response=json.dumps({"status": "success"}),
         status=200,
         mimetype='application/json'
     )
